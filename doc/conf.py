dd72a5ee94c8bf01b2901d5b55ee265aad7a8fb9
Sander Faas <s.faas@gymnasiumbeekvliet.nl>
2026-02-08 13:11:42 +0100

    Removed sphinx-design

--- doc/conf.py
+++ doc/conf.py
@@ -19,7 +19,7 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx_design']
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.venv']
