27f9f4260b97e821d6de1d351b71880b9a01c425
S. Faas <s.faas@gymnasiumbeekvliet.nl>
2026-06-09 10:13:39 +0200

    Try Furo theme

--- doc/conf.py
+++ doc/conf.py
@@ -29,7 +29,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.venv']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_book_theme'
+html_theme = 'furo'
 html_static_path = ['_static']
 html_css_files = ['custom.css']
 html_theme_options = {
