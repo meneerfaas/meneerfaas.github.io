f30dd6afde17b583c405aadb32b17119018108e3
S. Faas <s.faas@gymnasiumbeekvliet.nl>
2026-06-09 10:37:33 +0200

    Sidebar experiment

--- doc/conf.py
+++ doc/conf.py
@@ -32,7 +32,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.venv']
 html_theme = 'furo'
 html_static_path = ['_static']
 html_css_files = ['custom.css']
-html_logo = "_static/images/programmer_128.png"
+html_logo = "images/programmer_128.png"
 html_theme_options = {
     "sidebar_hide_name": True,
 }
